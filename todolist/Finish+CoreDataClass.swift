ccb942cfd5a2c19516b6793dde2a8bfa9eb605f9
FireTw0324 <102271545+FireTw0324@users.noreply.github.com>
2022-03-31 04:23:19 -0700

    Add files via upload

---
+++ todolist/Finish+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Finish+CoreDataClass.swift
+//  todolist
+//
+//  Created by HaoKai Lee on 2022/3/31.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Finish)
+public class Finish: NSManagedObject {
+
+}
