8ae1050671189dbe657714424e4125a282861b63
FireTw0324 <102271545+FireTw0324@users.noreply.github.com>
2022-03-31 04:45:25 -0700

    Add files via upload

---
+++ todolist/Event+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Event+CoreDataClass.swift
+//  todolist
+//
+//  Created by HaoKai Lee on 2022/3/31.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Event)
+public class Event: NSManagedObject {
+
+}
---
+++ todolist/Finish+CoreDataProperties.swift
@@ -0,0 +1,25 @@
+//
+//  Finish+CoreDataProperties.swift
+//  todolist
+//
+//  Created by HaoKai Lee on 2022/3/31.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Finish {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Finish> {
+        return NSFetchRequest<Finish>(entityName: "Finish")
+    }
+
+    @NSManaged public var title: String?
+
+}
+
+extension Finish : Identifiable {
+
+}
